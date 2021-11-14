67d0a932c3a2623d0587b60247529ea077d9874b
kewalb <bkewal@gmail.com>
2021-11-14 11:35:31 +0530

    INITIAL COMMIT

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { PriceCard1 } from "./PriceCard1";
+import { PriceCard2 } from "./PriceCard2";
+import { PriceCard3 } from "./PriceCard3";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
 
 function App() {
+  const mystyle = {
+    backgroundColor: "#007bff",
+    background: "linear-gradient(to right, #0062E6, #33AEFF)",
+    width:'auto',
+    minHeight: '104vh',
+    display: "flex",
+    borderRadius: 0,
+    textAlign: "center",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Paper elevation={1} style={mystyle} className="cardbg">
+      <Grid container spacing={{ xs: 1, md: 3 }} columns={{ xs: 1, sm: 8, md: 12 }}>
+        
+        <Grid item xs={2} sm={4} md={4}>
+          <PriceCard1 />
+        </Grid>
+        <Grid item xs={2} sm={4} md={4}>
+          <PriceCard2 />
+        </Grid>
+        <Grid item xs={2} sm={4} md={4}>
+          <PriceCard3 />
+        </Grid>
+       
+      </Grid>
+      </Paper>
   );
 }
 
---
+++ src/PriceCard2.js
@@ -0,0 +1,129 @@
+
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import CardContent from '@mui/material/CardContent';
+import ListItemText from '@mui/material/ListItemText';
+import CheckIcon from '@mui/icons-material/Check';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Button from '@mui/material/Button';
+import CloseIcon from '@mui/icons-material/Close';
+
+export function PriceCard2() {
+  
+  const divider = {
+    margin: "0.5rem"
+  };
+
+  const btn = {
+    marginTop: 8,
+    width: '100%',
+    fontSize: '90%',
+    borderRadius: '5rem',
+    letterSpacing: '.1rem',
+    fontWeight: 'bold',
+    padding: "1rem",
+    opacity: 0.7,
+    transition: "all 0.2s",
+  };
+
+  return (
+    
+      <Card style={{ margin: 20, borderRadius: 30, width: 'auto', display:'grid' }} className="card"
+      sx={{
+        ":hover": {
+          marginTop: "-.25rem",
+          marginBottom: ".25rem",
+          boxShadow: 20, // theme.shadows[20]
+        },
+      }}>
+        <CardHeader
+          title={<Typography style={{ color: "grey" }}>Plus</Typography>}
+          subheader={<Typography variant='h2'>$9<sub style={{ fontSize: 25 }}>/Month</sub></Typography>}
+          style={{ textAlign: 'center' }} />
+        <Divider style={divider} />
+        <CardContent>
+          <List style={{ margin: 0, padding: 1 }}>
+            <ListItem>
+              <ListItemIcon style={{ minWidth: 0 }}>
+                <CheckIcon sx={{ color: "black", fontSize: 25 }} />
+              </ListItemIcon>
+              <ListItemText
+                style={{ margin: 0 }}
+                primary={<Typography><b>5 Users</b></Typography>} />
+            </ListItem>
+
+            <ListItem>
+              <ListItemIcon style={{ minWidth: 0 }}>
+                <CheckIcon sx={{ color: "black", fontSize: 25 }} />
+              </ListItemIcon>
+              <ListItemText
+                style={{ margin: 0 }}
+                primary="50GB Storage" />
+            </ListItem>
+
+            <ListItem>
+              <ListItemIcon style={{ minWidth: 0 }}>
+                <CheckIcon sx={{ color: "black", fontSize: 25 }} />
+              </ListItemIcon>
+              <ListItemText
+                style={{ margin: 0 }}
+                primary="Unlimited Public Projects" />
+            </ListItem>
+
+            <ListItem>
+              <ListItemIcon style={{ minWidth: 0 }}>
+                <CheckIcon sx={{ color: "black", fontSize: 25 }} />
+              </ListItemIcon>
+              <ListItemText
+                style={{ margin: 0 }}
+                primary="Community Access" />
+            </ListItem>
+
+            <ListItem>
+              <ListItemIcon style={{ minWidth: 0 }}>
+                <CheckIcon sx={{ color: "black", fontSize: 25 }} />
+              </ListItemIcon>
+              <ListItemText
+                style={{ margin: 0 }}
+                primary="Unlimited Private Projects" />
+            </ListItem>
+
+            <ListItem>
+              <ListItemIcon style={{ minWidth: 0 }}>
+                <CheckIcon sx={{ color: "black", fontSize: 25 }} />
+              </ListItemIcon>
+              <ListItemText
+                style={{ margin: 0 }}
+                primary="Dedicated Phone Support" />
+            </ListItem>
+
+            <ListItem>
+              <ListItemIcon style={{ minWidth: 0 }}>
+                <CheckIcon sx={{ color: "black", fontSize: 25 }} />
+              </ListItemIcon>
+              <ListItemText
+                style={{ margin: 0 }}
+                primary="Free Subdomain" />
+            </ListItem>
+
+            <ListItem disabled>
+              <ListItemIcon style={{ minWidth: 0 }}>
+                <CloseIcon sx={{ color: "black", fontSize: 25 }} />
+              </ListItemIcon>
+              <ListItemText
+                style={{ margin: 0 }}
+                primary="Monthly Status Reports" />
+            </ListItem>
+
+          </List>
+
+          <Button variant="contained" style={btn}>Button</Button>
+        </CardContent>
+      </Card>
+    
+  );
+}
